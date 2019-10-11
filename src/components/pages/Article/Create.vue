49ae9b3d9957ef91825902871124118b9bc6174d
Nguyen Trong Nghia <nguyen.nghia@rivercrane.com.vn>
2019-10-11 08:23:20 +0700

    add date time picker

--- src/components/pages/Article/Create.vue
+++ src/components/pages/Article/Create.vue
@@ -19,13 +19,16 @@
             </div>
             <div class="form-group">
                 <label>Tag</label>
-                <select2
-                    id="selectTag"
-                />
+                <select2 id="selectTag" />
             </div>
             <div class="form-group">
                 <label>Content</label>
                 <editor v-model="article.content" :init="editorInit"></editor>
+            </div>
+            <div class="form-group">
+                <label>Created At</label>
+                <date-time-picker v-model="article.createdAt" :config="options"></date-time-picker>
+                <div>{{article.createdAt}} => Moment: {{ article.createdAt | moment("YYYY-MM-DD HH:mm") }}</div>
             </div>
             <div class="form-group">
                 <button class="btn btn-primary" v-on:click="createArticle">Save</button>
@@ -35,14 +38,9 @@
 </template>
 <script>
 import Axios from "axios";
-import Select2 from "v-select2-component";
-import Editor from '@tinymce/tinymce-vue';
 
 export default {
-    components: {
-        Select2,
-        Editor,
-    },
+    components: {},
     data() {
         return {
             loading: false,
@@ -52,34 +50,62 @@ export default {
                 category: [1, 2],
                 tag: [1, 2, 3],
                 content: "",
+                createdAt: new Date()
             },
             myOptions: [], // or [{id: key, text: value}, {id: key, text: value}]
             settingsCategory: {},
             editorInit: {
-                branding    : false,
-                skin        : 'lightgray',
-                plugins     : [ 'link', 'table', 'paste' ],
-                toolbar     : 'bold underline link table',
-                language    : 'ja',
+                branding: false,
+                skin: "lightgray",
+                plugins: ["link", "table", "paste"],
+                toolbar: "bold underline link table",
+                language: "ja",
                 menubar: false,
-                forced_root_block: '',
+                forced_root_block: "",
                 relative_urls: false,
                 convert_urls: false,
                 paste_as_text: true,
                 link_title: false,
                 link_class_list: [
-                    {title: 'アンダーラインなし', value: 's-article_textlink'}
+                    { title: "アンダーラインなし", value: "s-article_textlink" }
                 ],
                 table_default_attributes: {
-                    'class': 's-article_table'
+                    class: "s-article_table"
                 },
-                formats:{
-                    bold: { inline: 'span', attributes: { 'class': 's-article_textstrong' } },
-                    underline: { inline: 'span', attributes: { 'class': 's-article_p-marker' } }
+                formats: {
+                    bold: {
+                        inline: "span",
+                        attributes: { class: "s-article_textstrong" }
+                    },
+                    underline: {
+                        inline: "span",
+                        attributes: { class: "s-article_p-marker" }
+                    }
                 },
                 height: 200,
                 resize: false,
-                content_css: '/vender/css/editor.css?v=1.2'
+                content_css: "/vendor/css/editor.css?v=1.2"
+            },
+            options: {
+                format: "DD/MM/YYYY HH:mm",
+                useCurrent: false,
+                showClear: true,
+                showClose: true,
+                locale: "ja",
+                defaultDate: "11/1/2019",
+                disabledDates: [new Date(2019, 11 - 1, 21), "11/22/2019 00:53"],
+                // custom icons
+                // icons: {
+                //     time: "far fa-clock",
+                //     date: "far fa-calendar",
+                //     up: "fas fa-arrow-up",
+                //     down: "fas fa-arrow-down",
+                //     previous: "fas fa-chevron-left",
+                //     next: "fas fa-chevron-right",
+                //     today: "fas fa-calendar-check",
+                //     clear: "far fa-trash-alt",
+                //     close: "far fa-times-circle"
+                // }
             }
         };
     },
@@ -91,13 +117,16 @@ export default {
             // call api get data by id to get name
             Axios.get("https://jsonplaceholder.typicode.com/posts/" + id)
                 .then(res => {
-                    $("#selectCategory").append(new Option(res.data.title, id, true, true)).trigger('select');
+                    $("#selectCategory")
+                        .append(new Option(res.data.title, id, true, true))
+                        .trigger("select");
                 })
                 .catch(err => {});
         });
     },
     methods: {
         createArticle() {
+            this.$swal("Hello", "Hello brave new world!", "success");
             console.log("data create: ", this.article);
         },
         myChangeEvent(val) {
@@ -218,7 +247,7 @@ export default {
         },
         formatRepoSelection(repo) {
             return repo.full_name || repo.text;
-        },
+        }
     }
 };
 </script>
@@ -248,12 +277,15 @@ export default {
     color: #777;
     margin-top: 4px;
 }
-.select2-result-repository__forks, .select2-result-repository__stargazers, .select2-result-repository__watchers {
+.select2-result-repository__forks,
+.select2-result-repository__stargazers,
+.select2-result-repository__watchers {
     display: inline-block;
     color: #aaa;
     font-size: 11px;
 }
-.select2-result-repository__forks, .select2-result-repository__stargazers {
+.select2-result-repository__forks,
+.select2-result-repository__stargazers {
     margin-right: 1em;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,24 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from './router';
+import router from "./router";
 
-import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+// import common component global
+import BootstrapVue from "bootstrap-vue";
+
+import Editor from "@tinymce/tinymce-vue";
+import Select2 from "v-select2-component";
+import moment from "vue-moment";
+import VueSweetalert2 from "vue-sweetalert2";
+import DateTimePicker from "vue-bootstrap-datetimepicker";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "pc-bootstrap4-datetimepicker/build/css/bootstrap-datetimepicker.css";
 Vue.use(BootstrapVue);
+Vue.use(moment);
+Vue.use(VueSweetalert2);
+Vue.component("dateTimePicker", DateTimePicker);
+Vue.component("select2", Select2);
+Vue.component("editor", Editor);
 
 Vue.config.productionTip = false;
-
 new Vue({ router, render: h => h(App) }).$mount("#app");
